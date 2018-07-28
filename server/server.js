89aacf754953c3d2fc97bd259b351f04d1e30daa
Kennan <kennan.smith1@gmail.com>
2018-07-28 13:15:29 -0600

    halting for now - npm start fails and produces errors that I'm not yet sure how to correct - it could just be that the tutorial I am using is outdated or I don't have certain software installed - either way, I will likely end this project here and start over with a react app style that I am more familiar with

--- server/server.js
+++ server/server.js
@@ -1,12 +1,25 @@
 var express = require("express");
 var router = require("./routes/routes.js");
 var path = require("path");
+var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
 
 var app = express();
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "../client"));
-app.use(express.static(path.join(__dirname, "../client")));
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+} else {
+    app.use(express.static(path.join(__dirname, "../client")));
+
+}
+
+app.use(bodyParser.json({limit: "50mb"}));
+app.use(bodyParser.urlencoded({limit: "50mb", extended: false}));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/expenseManagerDb");
 
 app.use("/", router);
 
